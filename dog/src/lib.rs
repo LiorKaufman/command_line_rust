9fb2d6a0704cdd8e9577bd846ead6f197b460d48
lior@sightly.com <lior@sightly.com>
2022-10-23 11:48:24 -0700

    add file read logic

--- dog/src/lib.rs
+++ dog/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
-use std::{error::Error, vec};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -11,27 +13,54 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    // dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}",filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("dog")
         .version("0.1.0")
         .author("Lior Kaufman")
         .arg(
-            Arg::with_name("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .min_values(1),
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
         )
-        .about("like cat but it's made in rust and called dog")
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::with_name("number_nonblank")
+                .short("b")
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .about("Terminal application like cat but it's made in rust and called dog")
         .get_matches();
 
     Ok(Config {
-        files: vec![String::from("test")],
-        number_lines: false,
-        number_nonblank_lines: false,
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
     })
 }
